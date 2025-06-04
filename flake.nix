e8929db0264521a16f0812431435303963d8aa3e
readf0x <davis.a.forsythe@gmail.com>
2025-06-04 18:40:07 -0500

    update nix integration

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "readf0x's quickshell config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     quickshell = {
       url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,6 +12,11 @@
   outputs = { self, nixpkgs, quickshell }: let
     lib = nixpkgs.lib;
     perSystem = package: (lib.listToAttrs (lib.map (a: { name = a; value = package { pkgs = nixpkgs.legacyPackages.${a}; system = a; }; }) (lib.attrNames nixpkgs.legacyPackages)));
+    makeQmlPath = pkgs: lib.makeSearchPath "lib/qt-6/qml" (map (path: "${path}") pkgs);
+    qmlPath = pkgs: makeQmlPath [
+      quickshell.packages.${pkgs.system}.default
+      pkgs.kdePackages.full
+    ];
   in {
     packages = perSystem ({ pkgs, system }: rec {
       courier = pkgs.stdenv.mkDerivation (finalAttrs: {
@@ -49,6 +54,7 @@
         fi
         export FONTCONFIG_FILE=${pkgs.makeFontsConf { fontDirectories = [ courier ]; }}
         export PATH="${lib.makeBinPath dependencies}:$PATH"
+        export QML2_IMPORT_PATH="${qmlPath pkgs}"
         ${quickshell.packages.${system}.default}/bin/quickshell $@
       '';
       default = neofuturism-shell;
@@ -56,13 +62,10 @@
     devShells = perSystem ({ pkgs, system }: {
       default = pkgs.mkShell {
         packages = [
-          quickshell.packages.${system}.default
+          self.packages.${system}.default
         ];
 
-        QML2_IMPORT_PATH = lib.makeSearchPath "lib/qt-6/qml" [
-          "${quickshell.packages.${system}.default}"
-          "${pkgs.kdePackages.full}"
-        ];
+        QML2_IMPORT_PATH = qmlPath pkgs;
 
         shellHook = ''
           export QS_CONFIG_PATH="$(pwd)/src"
